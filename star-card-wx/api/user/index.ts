3b8592b09b7897bc64b119bf3b5c1a3393fa4597
钟金辰 <2541842721@qq.com>
2025-03-31 17:08:23 +0800

    同步

--- star-card-wx/api/user/index.ts
+++ star-card-wx/api/user/index.ts
@@ -1,9 +1,18 @@
 import ezInstance from "@/utils/ezInstance";
 import { login } from "@/api/login/index";
-const { createCurd } = ezInstance;
+const { ezclient } = ezInstance;
 
-const userFields = [
+const userFields =[
 	"id created_at user_id username mobile nickname avatar_id name sex birthday profile attach_data score user_inviteparent_user city role is_authentication is_personal is_recommend background_image_id province area location fz_distance_from_location address_detail house start_business_hours exp_business_hours address_title address_info growth_value edit_time"
 ]
 
-export const user = createCurd("user", userFields)
+export const getUserInfo = async () => {
+	const { userID } = await login()
+	const user = await ezclient.queryGetFirstOne({
+		name: "user",
+		args: { where: { id: { _eq: userID } } },
+		fields: userFields
+	})
+	console.log(user);
+	return user
+}
