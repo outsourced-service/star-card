f5b03182595ceac55ac0017b31725e8398aaf2fd
rad_fish <1640566213@qq.com>
2025-04-03 08:06:05 +0800

    订单详情页

---
+++ star-card-wx/pages/order/components/card-progress/card-progress.vue
@@ -0,0 +1,461 @@
+<template>
+	<view class="card-picture-list">
+		<uv-collapse ref="collapse" :value="value" :border="false" @close="onClose" @open="onOpen">
+			<uv-collapse-item title="进度详情" name="1" :border="value.includes('1')">
+				<uv-steps :current="current" direction="column" active-color="#fea800" active-icon="clock-fill">
+					<uv-steps-item customStyle="display: flex; align-items: baseline;">
+						<template v-slot:icon>
+							<uv-icon name="checkmark-circle-fill" color="#fea800" size="36rpx" v-if="current > 0"></uv-icon>
+							<uv-icon name="clock-fill" color="#fea800" size="36rpx" v-else-if="current == 0"></uv-icon>
+							<view v-else class="inactive-icon-circle"></view>
+						</template>
+						<template v-slot:title>
+							<view class="steps-active" v-if="current >= 0">
+								<view class="steps-active-title">订单创建，待用户寄出</view>
+								<view class="steps-active-time">2024-05-21</view>
+							</view>
+							<view class="steps-inactive" v-else>
+								<view class="steps-inactive-title">订单创建，待用户寄出</view>
+							</view>
+						</template>
+						<template v-slot:desc>
+							<view style="height: 36rpx;">
+							</view>
+						</template>
+					</uv-steps-item>
+					<uv-steps-item customStyle="display: flex; align-items: baseline;">
+						<template v-slot:icon>
+							<uv-icon name="checkmark-circle-fill" color="#fea800" size="36rpx" v-if="current > 1"></uv-icon>
+							<uv-icon name="clock-fill" color="#fea800" size="36rpx" v-else-if="current == 1"></uv-icon>
+							<view v-else class="inactive-icon-circle"></view>
+						</template>
+						<template v-slot:title>
+							<view class="steps-active" v-if="current >= 1">
+								<view class="steps-active-title">用户已寄出</view>
+								<view class="steps-active-time">2024-05-21</view>
+							</view>
+							<view class="steps-inactive" v-else>
+								<view class="steps-inactive-title">用户已寄出</view>
+							</view>
+						</template>
+						<template v-slot:desc>
+							<view :class="current >= 1 ? 'steps-desc-button' : 'steps-desc-text'">
+								SF14472839247865011
+							</view>
+						</template>
+					</uv-steps-item>
+					<uv-steps-item customStyle="display: flex; align-items: baseline;">
+						<template v-slot:icon>
+							<uv-icon name="checkmark-circle-fill" color="#fea800" size="36rpx" v-if="current > 2"></uv-icon>
+							<uv-icon name="clock-fill" color="#fea800" size="36rpx" v-else-if="current == 2"></uv-icon>
+							<view v-else class="inactive-icon-circle"></view>
+						</template>
+						<template v-slot:title>
+							<view class="steps-active" v-if="current >= 2">
+								<view class="steps-active-title">快递已签收</view>
+								<view class="steps-active-time">2024-05-21</view>
+							</view>
+							<view class="steps-inactive" v-else>
+								<view class="steps-inactive-title">快递已签收</view>
+							</view>
+						</template>
+						<template v-slot:desc>
+							<view style="height: 36rpx;">
+							</view>
+						</template>
+					</uv-steps-item>
+					<uv-steps-item customStyle="display: flex; align-items: baseline;">
+						<template v-slot:icon>
+							<uv-icon name="checkmark-circle-fill" color="#fea800" size="36rpx" v-if="current > 3"></uv-icon>
+							<uv-icon name="clock-fill" color="#fea800" size="36rpx" v-else-if="current == 3"></uv-icon>
+							<view v-else class="inactive-icon-circle"></view>
+						</template>
+						<template v-slot:title>
+							<view class="steps-active" v-if="current >= 3">
+								<view class="steps-active-title">已拆包入库，订单准备中</view>
+								<view class="steps-active-time">2024-05-21</view>
+							</view>
+							<view class="steps-inactive" v-else>
+								<view class="steps-inactive-title">已拆包入库，订单准备中</view>
+							</view>
+						</template>
+						<template v-slot:desc>
+							<view class="steps-desc-text">
+								若存在验品, 请等待我们联系您
+							</view>
+						</template>
+					</uv-steps-item>
+					<uv-steps-item customStyle="display: flex; align-items: baseline;">
+						<template v-slot:icon>
+							<uv-icon name="checkmark-circle-fill" color="#fea800" size="36rpx" v-if="current > 4"></uv-icon>
+							<uv-icon name="clock-fill" color="#fea800" size="36rpx" v-else-if="current == 4"></uv-icon>
+							<view v-else class="inactive-icon-circle"></view>
+						</template>
+						<template v-slot:title>
+							<view class="steps-active" v-if="current >= 4">
+								<view class="steps-active-title">已完成验品，待发车</view>
+								<view class="steps-active-time">2024-05-21</view>
+							</view>
+							<view class="steps-inactive" v-else>
+								<view class="steps-inactive-title">已完成验品，待发车</view>
+							</view>
+						</template>
+						<template v-slot:desc>
+							<view :class="current >= 4 ? 'steps-desc-button' : 'steps-desc-text'">
+								验品报告
+							</view>
+						</template>
+					</uv-steps-item>
+					<uv-steps-item customStyle="display: flex; align-items: baseline;">
+						<template v-slot:icon>
+							<uv-icon name="checkmark-circle-fill" color="#fea800" size="36rpx" v-if="current > 5"></uv-icon>
+							<uv-icon name="clock-fill" color="#fea800" size="36rpx" v-else-if="current == 5"></uv-icon>
+							<view v-else class="inactive-icon-circle"></view>
+						</template>
+						<template v-slot:title>
+							<view class="steps-active" v-if="current >= 5">
+								<view class="steps-active-title">已发车, 待抵达评级机构</view>
+								<view class="steps-active-time">2024-05-21</view>
+							</view>
+							<view class="steps-inactive" v-else>
+								<view class="steps-inactive-title">已发车, 待抵达评级机构</view>
+							</view>
+						</template>
+						<template v-slot:desc>
+							<view class="steps-desc-text">
+								一般将在2-5天内抵达
+							</view>
+						</template>
+					</uv-steps-item>
+					<uv-steps-item customStyle="display: flex; align-items: baseline;">
+						<template v-slot:icon>
+							<uv-icon name="checkmark-circle-fill" color="#fea800" size="36rpx" v-if="current > 6"></uv-icon>
+							<uv-icon name="clock-fill" color="#fea800" size="36rpx" v-else-if="current == 6"></uv-icon>
+							<view v-else class="inactive-icon-circle"></view>
+						</template>
+						<template v-slot:title>
+							<view class="steps-active" v-if="current >= 6">
+								<view class="steps-active-title">抵达机构 Arrived</view>
+								<view class="steps-active-time">2024-05-21</view>
+							</view>
+							<view class="steps-inactive" v-else>
+								<view class="steps-inactive-title">抵达机构 Arrived</view>
+							</view>
+						</template>
+						<template v-slot:desc>
+							<view style="height: 36rpx;">
+							</view>
+						</template>
+					</uv-steps-item>
+					<uv-steps-item customStyle="display: flex; align-items: baseline;">
+						<template v-slot:icon>
+							<uv-icon name="checkmark-circle-fill" color="#fea800" size="36rpx" v-if="current > 7"></uv-icon>
+							<uv-icon name="clock-fill" color="#fea800" size="36rpx" v-else-if="current == 7"></uv-icon>
+							<view v-else class="inactive-icon-circle"></view>
+						</template>
+						<template v-slot:title>
+							<view class="steps-active" v-if="current >= 7">
+								<view class="steps-active-title">信息核对中 Research&ID</view>
+								<view class="steps-active-time">2024-05-21</view>
+							</view>
+							<view class="steps-inactive" v-else>
+								<view class="steps-inactive-title">信息核对中 Research&ID</view>
+							</view>
+						</template>
+						<template v-slot:desc>
+							<view style="height: 36rpx;">
+							</view>
+						</template>
+					</uv-steps-item>
+					<uv-steps-item customStyle="display: flex; align-items: baseline;">
+						<template v-slot:icon>
+							<uv-icon name="checkmark-circle-fill" color="#fea800" size="36rpx" v-if="current > 8"></uv-icon>
+							<uv-icon name="clock-fill" color="#fea800" size="36rpx" v-else-if="current == 8"></uv-icon>
+							<view v-else class="inactive-icon-circle"></view>
+						</template>
+						<template v-slot:title>
+							<view class="steps-active" v-if="current >= 8">
+								<view class="steps-active-title">评分中 Grading</view>
+								<view class="steps-active-time">2024-05-21</view>
+							</view>
+							<view class="steps-inactive" v-else>
+								<view class="steps-inactive-title">评分中 Grading</view>
+							</view>
+						</template>
+						<template v-slot:desc>
+							<view style="height: 36rpx;">
+							</view>
+						</template>
+					</uv-steps-item>
+					<uv-steps-item customStyle="display: flex; align-items: baseline;">
+						<template v-slot:icon>
+							<uv-icon name="checkmark-circle-fill" color="#fea800" size="36rpx" v-if="current > 9"></uv-icon>
+							<uv-icon name="clock-fill" color="#fea800" size="36rpx" v-else-if="current == 9"></uv-icon>
+							<view v-else class="inactive-icon-circle"></view>
+						</template>
+						<template v-slot:title>
+							<view class="steps-active" v-if="current >= 9">
+								<view class="steps-active-title">封装中 Assembly</view>
+								<view class="steps-active-time">2024-05-21</view>
+							</view>
+							<view class="steps-inactive" v-else>
+								<view class="steps-inactive-title">封装中 Assembly</view>
+							</view>
+						</template>
+						<template v-slot:desc>
+							<view style="height: 36rpx;">
+							</view>
+						</template>
+					</uv-steps-item>
+					<uv-steps-item customStyle="display: flex; align-items: baseline;">
+						<template v-slot:icon>
+							<uv-icon name="checkmark-circle-fill" color="#fea800" size="36rpx" v-if="current > 10"></uv-icon>
+							<uv-icon name="clock-fill" color="#fea800" size="36rpx" v-else-if="current == 10"></uv-icon>
+							<view v-else class="inactive-icon-circle"></view>
+						</template>
+						<template v-slot:title>
+							<view class="steps-active" v-if="current >= 10">
+								<view class="steps-active-title">已出分, 待机构寄回 Shipping</view>
+								<view class="steps-active-time">2024-05-21</view>
+							</view>
+							<view class="steps-inactive" v-else>
+								<view class="steps-inactive-title">已出分, 待机构寄回 Shipping</view>
+							</view>
+						</template>
+						<template v-slot:desc>
+							<view style="height: 36rpx;">
+							</view>
+						</template>
+					</uv-steps-item>
+					<uv-steps-item customStyle="display: flex; align-items: baseline;">
+						<template v-slot:icon>
+							<uv-icon name="checkmark-circle-fill" color="#fea800" size="36rpx" v-if="current > 11"></uv-icon>
+							<uv-icon name="clock-fill" color="#fea800" size="36rpx" v-else-if="current == 11"></uv-icon>
+							<view v-else class="inactive-icon-circle"></view>
+						</template>
+						<template v-slot:title>
+							<view class="steps-active" v-if="current >= 11">
+								<view class="steps-active-title">机构已寄出 Shipped</view>
+								<view class="steps-active-time">2024-05-21</view>
+							</view>
+							<view class="steps-inactive" v-else>
+								<view class="steps-inactive-title">机构已寄出 Shipped</view>
+							</view>
+						</template>
+						<template v-slot:desc>
+							<view class="steps-desc-text">
+								一般将在5-10天内抵达
+							</view>
+						</template>
+					</uv-steps-item>
+					<uv-steps-item customStyle="display: flex; align-items: baseline;">
+						<template v-slot:icon>
+							<uv-icon name="checkmark-circle-fill" color="#fea800" size="36rpx" v-if="current > 12"></uv-icon>
+							<uv-icon name="clock-fill" color="#fea800" size="36rpx" v-else-if="current == 12"></uv-icon>
+							<view v-else class="inactive-icon-circle"></view>
+						</template>
+						<template v-slot:title>
+							<view class="steps-active" v-if="current >= 12">
+								<view class="steps-active-title">抵达星卡仓库, 打包中</view>
+								<view class="steps-active-time">2024-05-21</view>
+							</view>
+							<view class="steps-inactive" v-else>
+								<view class="steps-inactive-title">抵达星卡仓库, 打包中</view>
+							</view>
+						</template>
+						<template v-slot:desc>
+							<view class="steps-desc-text">
+								我们将尽量在1-3工作日内发出
+							</view>
+						</template>
+					</uv-steps-item>
+					<uv-steps-item customStyle="display: flex; align-items: baseline;">
+						<template v-slot:icon>
+							<uv-icon name="checkmark-circle-fill" color="#fea800" size="36rpx" v-if="current > 13"></uv-icon>
+							<uv-icon name="clock-fill" color="#fea800" size="36rpx" v-else-if="current == 13"></uv-icon>
+							<view v-else class="inactive-icon-circle"></view>
+						</template>
+						<template v-slot:title>
+							<view class="steps-active" v-if="current >= 13">
+								<view class="steps-active-title">已寄回玩家</view>
+								<view class="steps-active-time">2024-05-21</view>
+							</view>
+							<view class="steps-inactive" v-else>
+								<view class="steps-inactive-title">已寄回玩家</view>
+							</view>
+						</template>
+						<template v-slot:desc>
+							<view style="height: 36rpx;">
+							</view>
+						</template>
+					</uv-steps-item>
+					<uv-steps-item customStyle="display: flex; align-items: baseline;">
+						<template v-slot:icon>
+							<uv-icon name="checkmark-circle-fill" color="#fea800" size="36rpx" v-if="current > 14"></uv-icon>
+							<uv-icon name="clock-fill" color="#fea800" size="36rpx" v-else-if="current == 14"></uv-icon>
+							<view v-else class="inactive-icon-circle"></view>
+						</template>
+						<template v-slot:title>
+							<view class="steps-active" v-if="current >= 14">
+								<view class="steps-active-title">玩家已签收，订单完成</view>
+								<view class="steps-active-time">2024-05-21</view>
+							</view>
+							<view class="steps-inactive" v-else>
+								<view class="steps-inactive-title">玩家已签收，订单完成</view>
+							</view>
+						</template>
+						<template v-slot:desc>
+							<view style="height: 36rpx;">
+							</view>
+						</template>
+					</uv-steps-item>
+				</uv-steps>
+			</uv-collapse-item>
+		</uv-collapse>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "card-pictureList",
+		props: {
+			data: {
+				type: Object,
+				default: () => ({})
+			}
+		},
+		options: {
+			styleIsolation: 'shared'
+		},
+		data() {
+			return {
+				value: ['1'],
+				current: 7,
+				stepsList: [
+					{
+						title: '订单创建，待用户寄出', desc: '', time: '2024-05-21'
+					}, {
+						title: '用户已寄出', desc: '', time: '2024-05-21'
+					}, {
+						title: '快递已签收', desc: '', time: '2024-05-21'
+					}, {
+						title: '已拆包入库，订单准备中', desc: '', time: '2024-05-21'
+					}, {
+						title: '已完成验品，待发车', desc: '', time: '2024-05-21'
+					}, {
+						title: '已发车, 待抵达评级机构', desc: '', time: '2024-05-21'
+					}, {
+						title: '抵达机构 Arrived', desc: '', time: '2024-05-21'
+					}, {
+						title: '信息核对中 Research&ID', desc: '', time: '2024-05-21'
+					}, {
+						title: '评分中 Grading', desc: '', time: ''
+					}, {
+						title: '封装中 Assembly', desc: '', time: ''
+					}, {
+						title: '已出分, 待机构寄回 Shipping', desc: '', time: ''
+					}, {
+						title: '机构已寄出 Shipped', desc: '', time: ''
+					}, {
+						title: '抵达星卡仓库, 打包中', desc: '', time: ''
+					}, {
+						title: '已寄回玩家', desc: '', time: ''
+					}, {
+						title: '玩家已签收，订单完成', desc: '', time: ''
+					}
+				]
+			};
+		},
+		methods: {
+			
+			onOpen(v) {
+				this.value.push(v)
+			},
+			onClose(v) {
+				this.value = this.value.find(i => i != v) || []
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.card-picture-list {
+		width: 100%;
+		min-height: 62.98rpx;
+		border-radius: 32rpx;
+		box-shadow: 0px 2px 8px 0px #0000001A;
+
+		::v-deep .uv-collapse-item__content__text {
+			padding: 16px;
+		}
+
+		::v-deep .uv-cell__body {
+			padding: 38.17rpx 22.9rpx 22.9rpx 22.9rpx;
+
+			.uv-cell__title {
+				flex-direction: row;
+				align-items: center;
+
+				.uv-cell__title-text {
+					color: #000000FC;
+					font-family: PingFang SC;
+					font-weight: 600;
+					font-size: 32rpx;
+					line-height: 40.08rpx;
+					margin-right: 8rpx;
+				}
+			}
+		}
+	}
+	
+	.steps-active {
+		display: flex;
+		justify-content: space-between;
+	}
+	
+	.steps-active-title {
+		font-size: 26rpx;
+		color: rgba(0, 0, 0, 0.66);
+	}
+	
+	.steps-active-time {
+		font-size: 26rpx;
+		color: rgba(0, 0, 0, 0.2);
+	}
+	
+	.steps-inactive-title {
+		font-size: 26rpx;
+		color: rgba(0, 0, 0, 0.33);
+	}
+	
+	.steps-desc-button {
+		font-weight: 600;
+		font-size: 22rpx;
+		line-height: 140%;
+		text-decoration: underline;
+		text-decoration-style: solid;
+		text-decoration-offset: 0%;
+		text-decoration-thickness: 0%;
+		color: rgba(254, 168, 0, 1);
+		margin-top: 4rpx;
+	}
+	
+	.steps-desc-text {
+		margin-top: 4rpx;
+		font-size: 22rpx;
+		line-height: 140%;
+		letter-spacing: 0%;
+		color: rgba(0, 0, 0, 0.33);
+		height: 36rpx;
+	}
+	
+	.inactive-icon-circle {
+		width: 32rpx;
+		height: 32rpx;
+		gap: 16rpx;
+		border-radius: 200rpx;
+		border-width: 2rpx;
+		background: rgba(242, 243, 246, 1);
+		border: 2rpx solid rgba(209, 209, 214, 1);
+	}
+</style>
--- star-card-wx/pages/order/index/index.vue
+++ star-card-wx/pages/order/index/index.vue
@@ -1,29 +1,138 @@
 <template>
-	<view class="container">
-		
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
+	<view class="container" :style="dynamicBackground">
+		<view class="container-order-info">
+			<view class="order-info-top">
+				<view class="info-top-tag">
+					<uv-image radius="12rpx" :src="orderInfo.evaluation?.order_logo_img.url" width="280rpx" height="56rpx"></uv-image>
+					<view class="top-tag-title">{{orderInfo.evaluation?.order_title}}</view>
+				</view>
+				<view class="info-top-item">
+					<view class="top-item-name">中速换评档 Crossover</view>
+					<view class="top-item-data">常规尺寸 / 仅评卡品 · 30张</view>
+				</view>
+			</view>
+			<cardOrderInfo :orderInfo="orderInfo" @handleElectronicStub="handleElectronicStub"></cardOrderInfo>
+		</view>
+		<view class="container-other-info">
+			<cardPictureList></cardPictureList>
+			<cardAddress></cardAddress>
+			<cardProgress></cardProgress>
+		</view>
 	</view>
 </template>
 
 <script>
+	import orderInfo from "@/api/order";
+	import cardOrderInfo from "../components/card-orderInfo/card-orderInfo.vue";
+	import cardAddress from "../components/card-address/card-address.vue";
+	import cardPictureList from "../components/card-pictureList/card-pictureList.vue";
+	import cardProgress from "../components/card-progress/card-progress.vue";
+	
 	export default {
-		data() {
-			return {
-				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+		components: {
+			cardOrderInfo,
+			cardAddress,
+			cardPictureList,
+			cardProgress
+		},
+		options: {
+		    styleIsolation: 'shared'
+		},
+		computed: {
+			dynamicBackground() {
+				// 动态设置背景图片
+				return {
+					'background-image': `url(${this.orderInfo.evaluation?.img.url})`,
+					'background-size': 'contain',
+					'background-position': 'top',
+					'background-repeat': 'no-repeat'
+				};
 			}
 		},
+		data() {
+			return {
+				orderInfo: {}
+			}
+		},
+		emit: [],
 		methods: {
-
+			handleElectronicStub() {
+				
+			},
+			getOrderInfoDetail(id) {
+				orderInfo.getOrderInfoDetail(id).then(res => {
+					this.orderInfo = res[0];
+				});
+			}
+		},
+		onLoad(option) {
+			this.getOrderInfoDetail(option.order_info_id);
+		},
+		mounted() {
 		}
 	}
 </script>
 
-<style>
+<style lang="scss" scoped>
 	.container {
-		padding: 20px;
-		font-size: 14px;
-		line-height: 24px;
+		width: 100vw;
+		height: 100vh;
+		background-color: #fff;
+		padding: 40rpx 32rpx;
+		display: flex;
+		flex-direction: column;
+		gap: 24rpx;
+	}
+	
+	.container-order-info {
+		display: flex;
+		flex-direction: column;
+		gap: 40rpx;
+	}
+	
+	.order-info-top {
+		display: flex;
+		flex-direction: column;
+		gap: 24rpx;
+	}
+	
+	.info-top-tag {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	
+	.top-tag-title {
+		font-size: 22rpx;
+		color: rgba(0, 0, 0, 0.33);
+		padding: 6rpx 20rpx;
+		border-radius: 2000rpx;
+		background: rgba(0, 0, 0, 0.03);
+	}
+	
+	.info-top-item {
+		display: flex;
+		flex-direction: column;
+		gap: 4rpx;
+	}
+	
+	.top-item-name {
+		font-weight: 600;
+		font-size: 44rpx;
+		line-height: 68rpx;
+		color: rgba(0, 0, 0, 0.9);
+	}
+	
+	.top-item-data {
+		font-weight: 500;
+		line-height: 32rpx;
+		font-size: 26rpx;
+		color: rgba(0, 0, 0, 0.66);
+	}
+	
+	.container-other-info {
+		display: flex;
+		flex-direction: column;
+		gap: 24rpx;
 	}
 </style>
