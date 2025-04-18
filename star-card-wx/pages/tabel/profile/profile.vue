3fd6ce865becd4801ecad0f13d745e5a63d3350e
钟金辰 <2541842721@qq.com>
2025-04-18 19:18:23 +0800

    bug 维修

--- star-card-wx/pages/tabel/profile/profile.vue
+++ star-card-wx/pages/tabel/profile/profile.vue
@@ -3,12 +3,11 @@
 		<!-- 顶部导航栏 -->
 		<navbar title=''></navbar>
 		<topInfoShow :data="userInfo"></topInfoShow>
+		<quickCard></quickCard>
 		<view class="page-item">
-			<quickCard></quickCard>
 			<cardCabinet :data="cardList" :number="userInfo.new_card_number"></cardCabinet>
 			<functionList></functionList>
 		</view>
-		<!-- <tabBar :value='4' /> -->
 		<tabBar value='我的' />
 	</view>
 </template>
@@ -18,7 +17,9 @@
 	import quickCard from '/pages/profile/components/quickCard.vue';
 	import cardCabinet from '/pages/profile/components/cardCabinet.vue';
 	import functionList from '/pages/profile/components/functionList.vue';
-	import { user } from '@/api/gloabal/index';
+	import {
+		user
+	} from '@/api/gloabal/index';
 
 	import {
 		userInfo
@@ -43,232 +44,6 @@
 		onShow() {
 			user.getUserInfo().then(res => {
 				this.userInfo = res
-			})
-
-		},
-		methods: {},
-		mounted() {}
-	}
-</script>
-
-<style lang="scss" scoped>
-	.page {
-		width: 100vw;
-		background: linear-gradient(180deg, #FECB66 0%, #FFFFFF 54.32%);
-	}
-
-	.page-item {
-		display: flex;
-		flex-direction: column;
-		background-color: #fff;
-		gap: 16rpx !important;
-		padding: 32rpx 24rpx;
-		border-top-left-radius: 40rpx;
-		border-top-right-radius: 40rpx;
-		margin-top: -20rpx;
-		box-shadow: 0rpx 4rpx 40rpx 0rpx rgba(0, 0, 0, 0.05);
-	}
-</style><template>
-	<view class="page">
-		<!-- 顶部导航栏 -->
-		<navbar title=''></navbar>
-		<topInfoShow :data="userInfo"></topInfoShow>
-		<view class="page-item">
-			<quickCard></quickCard>
-			<cardCabinet :data="cardList" :number="userInfo.new_card_number"></cardCabinet>
-			<functionList></functionList>
-		</view>
-		<!-- <tabBar :value='4' /> -->
-		<tabBar :value='2' />
-	</view>
-</template>
-
-<script>
-	import topInfoShow from '/pages/profile/components/topInfoShow.vue';
-	import quickCard from '/pages/profile/components/quickCard.vue';
-	import cardCabinet from '/pages/profile/components/cardCabinet.vue';
-	import functionList from '/pages/profile/components/functionList.vue';
-	import { user } from '@/api/gloabal/index';
-
-	import userCardCabinet from '@/api/card/userCardCabinet';
-	
-	import {
-		userInfo
-	} from '/mock/users';
-	import {
-		cardList
-	} from '/mock/cards';
-
-	export default {
-		components: {
-			topInfoShow,
-			quickCard,
-			cardCabinet,
-			functionList
-		},
-		data() {
-			return {
-				userInfo: userInfo,
-				cardList: cardList
-			}
-		},
-		onShow() {
-			user.getUserInfo().then(res => {
-				this.userInfo = res
-			})
-
-			userCardCabinet.getCardCabinetDetail().then(res => {
-				this.cardList = res
-			})
-		},
-		methods: {},
-		mounted() {}
-	}
-</script>
-
-<style lang="scss" scoped>
-	.page {
-		width: 100vw;
-		background: linear-gradient(180deg, #FECB66 0%, #FFFFFF 54.32%);
-	}
-
-	.page-item {
-		display: flex;
-		flex-direction: column;
-		background-color: #fff;
-		gap: 16rpx !important;
-		padding: 32rpx 24rpx;
-		border-top-left-radius: 40rpx;
-		border-top-right-radius: 40rpx;
-		margin-top: -20rpx;
-		box-shadow: 0rpx 4rpx 40rpx 0rpx rgba(0, 0, 0, 0.05);
-	}
-</style><template>
-	<view class="page">
-		<!-- 顶部导航栏 -->
-		<navbar title=''></navbar>
-		<topInfoShow :data="userInfo"></topInfoShow>
-		<view class="page-item">
-			<quickCard></quickCard>
-			<cardCabinet :data="cardList" :number="userInfo.new_card_number"></cardCabinet>
-			<functionList></functionList>
-		</view>
-		<!-- <tabBar :value='4' /> -->
-		<tabBar :value='2' />
-	</view>
-</template>
-
-<script>
-	import topInfoShow from '/pages/profile/components/topInfoShow.vue';
-	import quickCard from '/pages/profile/components/quickCard.vue';
-	import cardCabinet from '/pages/profile/components/cardCabinet.vue';
-	import functionList from '/pages/profile/components/functionList.vue';
-	import { user } from '@/api/gloabal/index';
-
-	import userCardCabinet from '@/api/card/userCardCabinet';
-	
-	import {
-		userInfo
-	} from '/mock/users';
-	import {
-		cardList
-	} from '/mock/cards';
-
-	export default {
-		components: {
-			topInfoShow,
-			quickCard,
-			cardCabinet,
-			functionList
-		},
-		data() {
-			return {
-				userInfo: userInfo,
-				cardList: cardList
-			}
-		},
-		onShow() {
-			user.getUserInfo().then(res => {
-				this.userInfo = res
-			})
-
-			userCardCabinet.getCardCabinetDetail().then(res => {
-				this.cardList = res
-			})
-		},
-		methods: {},
-		mounted() {}
-	}
-</script>
-
-<style lang="scss" scoped>
-	.page {
-		width: 100vw;
-		background: linear-gradient(180deg, #FECB66 0%, #FFFFFF 54.32%);
-	}
-
-	.page-item {
-		display: flex;
-		flex-direction: column;
-		background-color: #fff;
-		gap: 16rpx !important;
-		padding: 32rpx 24rpx;
-		border-top-left-radius: 40rpx;
-		border-top-right-radius: 40rpx;
-		margin-top: -20rpx;
-		box-shadow: 0rpx 4rpx 40rpx 0rpx rgba(0, 0, 0, 0.05);
-	}
-</style><template>
-	<view class="page">
-		<!-- 顶部导航栏 -->
-		<navbar title=''></navbar>
-		<topInfoShow :data="userInfo"></topInfoShow>
-		<view class="page-item">
-			<quickCard></quickCard>
-			<cardCabinet :data="cardList" :number="userInfo.new_card_number"></cardCabinet>
-			<functionList></functionList>
-		</view>
-		<!-- <tabBar :value='4' /> -->
-		<tabBar :value='2' />
-	</view>
-</template>
-
-<script>
-	import topInfoShow from '/pages/profile/components/topInfoShow.vue';
-	import quickCard from '/pages/profile/components/quickCard.vue';
-	import cardCabinet from '/pages/profile/components/cardCabinet.vue';
-	import functionList from '/pages/profile/components/functionList.vue';
-	import { user } from '@/api/gloabal/index';
-
-	import userCardCabinet from '@/api/card/userCardCabinet';
-	
-	import {
-		userInfo
-	} from '/mock/users';
-	import {
-		cardList
-	} from '/mock/cards';
-
-	export default {
-		components: {
-			topInfoShow,
-			quickCard,
-			cardCabinet,
-			functionList
-		},
-		data() {
-			return {
-				userInfo: userInfo,
-				cardList: cardList
-			}
-		},
-		onShow() {
-			user.getUserInfo().then(res => {
-				this.userInfo = res
-			})
-
-			userCardCabinet.getCardCabinetDetail().then(res => {
-				this.cardList = res
 			})
 		},
 		methods: {},
